76045b11fd3438eb3b3b5758d0488b006ba595b6
Kerri-Ann <kaybee1jam@yahoo.com>
2019-09-19 16:39:25 -0400

    set up test cases to test code

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -69,3 +69,62 @@ class ProjectManager extends Instructor {
     }
 }
 
+
+// Test Cases
+// Person test.
+const person1 = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+});
+
+console.log('-------- Person ---------');
+console.log(person1.speak());
+
+// Student test.
+const student1 = new Student({
+    name: 'Kerri-Ann',
+    location: 'Florida',
+    age: 30,
+    previousBackground: '2 year college',
+    className: 'WEB24',
+    favSubjects: ['HTML', 'CSS', 'Javascript']
+});
+
+console.log('-------- Student ---------');
+console.log(student1.speak());
+student1.listsSubjects()
+console.log(student1.PRAssignment('CSS'));
+console.log(student1.sprintChallenge('Javascript'));
+
+// Instructor test.
+const instructor1 = new Instructor({
+    name: 'Brit',
+    location: 'Canada',
+    age: 35,
+    specialty: 'Frontend',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'You got it!'
+});
+
+console.log('-------- Instructor ---------');
+console.log(instructor1.speak());
+console.log(instructor1.demo('Javascript'));
+console.log(instructor1.grade(student1, 'Javascript'));
+
+// PM test.
+const pm = new ProjectManager({
+    name: 'Juan',
+    location: 'Texas',
+    age: 30,
+    specialty: 'Frontend',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'You got it!',
+    gradClassName: 'WEB1',
+    favInstructor: 'Dan'
+});
+
+console.log('-------- Project Manager ---------');
+console.log(pm.speak());
+console.log(pm.standUp('Web_24'));
+console.log(pm.debugsCode(student1, 'Javascript'));
